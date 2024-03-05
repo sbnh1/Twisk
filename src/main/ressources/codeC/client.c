6adb200611d4df062778a3dfd95700f096a9619a
sbnh1 <saban.houssein1@gmail.com>
2024-03-05 14:43:28 +0100

    client.c fonction simulation

--- src/main/ressources/codeC/client.c
+++ src/main/ressources/codeC/client.c
@@ -3,9 +3,25 @@
 
 #include "def.h"
 
-void simulation(int ids){}
+void simulation(int ids){
+
+    int sasEntree = 0;
+    int activite = 1;
+    int sasSortie = 2;
+
+    entrer(sasEntree);
+    printf("j'entre dans le sas d'entrée");
+    delai(1, 0);
+    printf("je quitte le sas d'entrée");
+    transfert(sasEntree, activite);
+    printf("j'entre dans l'activité");
+    delai(1, 0);
+    printf("j'quitte l'activité");
+    transfert(activite, sasSortie);
+    printf("j'entre dans le sas de sortie");
+}
 
 int main(int argc, char** argv){
-    printf("1\n");
+    simulation(0);
     return 0;
 }
