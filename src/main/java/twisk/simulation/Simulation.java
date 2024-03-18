29592289f74837912a708a835225e9f615a60c20
sbnh1 <saban.houssein1@gmail.com>
2024-03-18 17:36:44 +0100

    simulation

--- src/main/java/twisk/simulation/Simulation.java
+++ src/main/java/twisk/simulation/Simulation.java
@@ -1,10 +1,26 @@
 package main.java.twisk.simulation;
 
-import main.java.twisk.monde.Monde;
+import main.java.twisk.monde.*;
+
 public class Simulation {
     public Simulation(){ }
     public void simuler(Monde monde){
         System.out.println("Vous etes dans une simulation :\n");
+
+        //création des étapes du monde
+        SasEntree sasEntree = new SasEntree("SasEntree");
+        Guichet guichet = new Guichet("Guichet");
+        ActiviteRestreinte activiteRestreinte = new ActiviteRestreinte("ActiviteRestreinte");
+        SasSortie sasSortie = new SasSortie("SasSortie");
+
+        //ajout des étapes au monde
+        monde.aCommeEntree(sasEntree);
+        monde.ajouter(guichet, activiteRestreinte);
+        monde.aCommeSortie(sasSortie);
+        sasEntree.ajouterSuccesseur(guichet);
+        guichet.ajouterSuccesseur(activiteRestreinte);
+        activiteRestreinte.ajouterSuccesseur(sasSortie);
+
         System.out.println(monde.toC());
     }
 }
